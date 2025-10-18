f4b93b06179b431ebf0fc1a675e793376654a8b9
MD.Shimul <miashimul6363@gmail.com>
2025-10-18 20:17:46 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#F59E0B',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
